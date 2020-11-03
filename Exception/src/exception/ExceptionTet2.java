d99f7639ee7caddfda69137d919eee32c5b22ebb
yeonho <3333hero@naver.com>
2020-11-03 15:01:34 +0900

    소스 수정

--- Exception/src/exception/ExceptionTet2.java
+++ Exception/src/exception/ExceptionTet2.java
@@ -4,6 +4,13 @@ public class ExceptionTet2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String data1 = "100";
+		String data2 = "a100";
+		
+		int value1 = Integer.parseInt(data1);
+		int value2 = Integer.parseInt(data2);
+		
+		System.out.println("value1 + value2");
 
 	}
 
